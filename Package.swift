c36692138acd348daeaa15831b37dbcb2a23c33c
Hunter Baker <62899372+literally-anything@users.noreply.github.com>
2024-07-28 15:09:38 -0400

    Fix dependencies in Package Description and enable swift 6 language mode

--- Package.swift
+++ Package.swift
@@ -1,25 +1,40 @@
 // swift-tools-version: 6.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftSpatial",
-    platforms: [.macOS(.v15)],
+    platforms: [
+        .macOS(.v14),
+        .custom("linux", versionString: "")
+    ],
     products: [
         .library(
             name: "SwiftSpatial",
             targets: ["SwiftSpatial"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/keyvariable/kvSIMD.swift.git", from: "1.0.3")
+        .package(url: "https://github.com/keyvariable/kvSIMD.swift.git", from: "1.0.3"),
+        .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
     ],
     targets: [
         .target(
-            name: "SwiftSpatial"),
+            name: "SwiftSpatial",
+            dependencies: [
+                .product(name: "kvSIMD", package: "kvSIMD.swift")
+            ],
+            swiftSettings: [
+                .swiftLanguageVersion(.v6),
+                .unsafeFlags(["-O", "-whole-module-optimization"], .when(configuration: .release)),
+                .unsafeFlags(["-Onone"], .when(configuration: .debug)),
+            ]
+        ),
         .testTarget(
             name: "SwiftSpatialTests",
-            dependencies: ["SwiftSpatial"]
+            dependencies: [
+                "SwiftSpatial",
+                .product(name: "Testing", package: "swift-testing"),
+            ]
         ),
     ]
 )
